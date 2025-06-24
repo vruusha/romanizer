af95f4fb3393af252eecbeffc0e6cf58d9abde8a
Vrushali Sawant <vrushali.sawant.v@gmail.com>
2025-06-24 15:11:06 -0400

    Rename frontend, clean node_modules, and update references

--- backend/logger.js
+++ backend/logger.js
@@ -29,18 +29,18 @@ const logger = createLogger({
     // - Write all logs with importance level of `error` or higher to `error.log`
     //   (i.e., error, fatal, but not other levels)
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'error.log', level: 'error' }),
     //
     // - Write all logs with importance level of `info` or higher to `combined.log`
     //   (i.e., fatal, error, warn, and info, but not trace)
     //
-    new winston.transports.File({ filename: 'combined.log' }),
+    new transports.File({ filename: 'combined.log' }),
   ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
+  logger.add(new transports.Console({
+    format: format.simple(),
   }));
 }
 
